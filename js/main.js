eedf402b65990f257a7de8ed2e3f8fb85b711e55
Daniel Luty <lutydaniel@gmail.com>
2018-11-12 07:07:29 +0100

    made header responsive, added calculator

--- js/main.js
+++ js/main.js
@@ -3,25 +3,71 @@
 $(function () {
 
     // Nav toggler
-    $('.hb-button').click(function() {
-        $('.nav-ul').slideToggle();
-    });
+    $('.hb-button')
+        .click(function () {
+            $('.nav-ul').slideToggle();
+        });
 
-    $('.nav-ul a, .logo').click(function() {
+    $('.nav-ul a, .logo').click(function () {
         $('.nav-ul').slideUp();
     })
 
     // Smooth scrolling
-    $('a[href*="#"]').click(function(e) {
-        if(this.hash !== "") {
+    $('a[href*="#"]').click(function (e) {
+        if (this.hash !== "") {
             e.preventDefault();
             let hash = this.hash;
 
             $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function() {
+                scrollTop: $(hash)
+                    .offset()
+                    .top
+            }, 800, function () {
                 window.location.hash = hash;
             });
         }
     });
 });
+
+/* *** Cennik Transportow *** */
+
+let formTrans = document.getElementById('form-trans');
+
+formTrans.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let cena = 0;
+    let km = document.getElementById('km').value;
+    let cenaTrans = document.getElementById('cenaTrans');
+    let poleCena = document.querySelector('.cena-trans');
+
+    if (km > 0 && km < 10000) {
+        km = km - 3;
+        cena = km * 5 + 50;
+        poleCena.style = 'display: block;'
+        cenaTrans.innerHTML = `${cena} zł`;
+        
+    } else {
+        cenaTrans.innerHTML = 'Podaj poprawną wartość';
+        km = 0;
+    }
+})
+
+/* *** Cennik Przeprowadzek *** */
+
+const formPrzep = document.getElementById('form-przep');
+
+formPrzep.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let cena = 0;
+    let hours = document.getElementById('hours').value;
+    let workers = document.querySelector('#workers').value;
+    let cenaPrzep = document.getElementById('cenaPrzep');
+    let poleCena = document.querySelector('.cena-przep');
+
+    cena = hours * workers;
+    
+    poleCena.style = 'display: block;'
+    cenaPrzep.innerHTML = `${cena} zł`;
+})
